4dfc852643f73a3ae5c5df75887d93622787b071
nottene <59664794+nottene@users.noreply.github.com>
2022-09-13 12:33:00 -0700

    Add files via upload

---
+++ review.java
@@ -0,0 +1,7 @@
+public class review {
+    public static void main(String args[]) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, my name is Charles Shi.");
+        System.out.println("I took this course because I find programming an interesting experience and I also want to enjoy that experience with my friends.");
+    }
+}
